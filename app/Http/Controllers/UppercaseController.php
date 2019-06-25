df4b26e6e6a7029c5d9f7c7989086508257cc4ca
Tarasovych <tarasovych.ua@gmail.com>
2019-06-25 11:13:26 +0300

    Initial commit

---
+++ app/Http/Controllers/UppercaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Uppercase;
+
+class UppercaseController extends Controller
+{
+    public function uppercase(Uppercase $request)
+    {
+
+    }
+}
